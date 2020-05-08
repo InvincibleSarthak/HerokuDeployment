38da302cbbd08461862a2fdd9e7a66e9fb343bbd
Sarthak Sharma <eccentric.sarthak@gmail.com>
2020-05-08 19:13:17 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,82 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req,res) => {
+    //console.log(req.url);
+    // if(req.url === '/'){
+    //     fs.readFile(path.join(__dirname, 'public', 'index.html'), (err,data) =>{
+    //         if(err) throw err;
+    //         res.writeHead(200, {'Content-Type': 'text/html'})
+    //         res.end(data);
+    //     })   
+    // }
+    // if(req.url === '/about'){
+    //     fs.readFile(path.join(__dirname, 'public', 'about.html'), (err,data) =>{
+    //         if(err) throw err;
+    //         res.writeHead(200, {'Content-Type': 'text/html'})
+    //         res.end(data);
+    //     })   
+    // }
+    // if(req.url === '/api/users'){
+    //     const users = [
+    //         {name: 'Sarthak', age: 20}
+    //     ];
+    //     res.writeHead(200, { 'Content-Type': 'application/json' });
+    //     res.end(JSON.stringify(users));
+    // }
+
+    //BUild file path
+    let filePath = path.join(
+        __dirname, 
+        'public', 
+        req.url === '/' ? 'index.html' : req.url
+        );
+
+    //Extension of file
+    let extname = path.extname(filePath);    
+
+    //Initial content type
+    let contentType = 'text/html';
+
+    //check ext and set content type
+    switch(extname){
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+
+    //Read File
+    fs.readFile(filePath, (err,content) => {
+        // console.log(err.code)
+        if(err){
+            if(err.code == 'ENOENT'){
+                fs.readFile(path.join(__dirname,'public','404.html'),
+                (err,content)=>{
+                    if(err) throw err;
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(content,'utf-8');
+                });
+            }
+            else{
+                //Some server error
+                res.writeHead(500);
+                res.end('Server Error '+ err.code);
+            }
+        }
+        else{
+            res.writeHead(200, {'Content-Type': contentType});
+            res.end(content, 'utf-8');
+        }
+    })
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log('Server running on port: ' + PORT));
+
+//node index requires to close the server after every change and then restart it
+//nodemon does everything side by side 
